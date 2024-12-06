e3c588d1faf075b932d64a1c04370e108eec8ef2
nzrir <ikarosli1503@gmail.com>
2024-12-06 18:01:06 +0000

    updated
    hopefully can run

--- prototypes/src/main/java/com/example/prototypes/HelloController.java
+++ prototypes/src/main/java/com/example/prototypes/HelloController.java
@@ -24,9 +24,9 @@ public class HelloController {
     private DbFunction db = new DbFunction();
     private Connection conn;
 
-    String DBNAME = "prototype2";
-    String USERNAME = "bek2";
-    String PASSWORD = "1234";
+    String DBNAME = "gamification";
+    String USERNAME = "postgres";
+    String PASSWORD = "fullstack24";
 
     @FXML
     public void initialize() {
