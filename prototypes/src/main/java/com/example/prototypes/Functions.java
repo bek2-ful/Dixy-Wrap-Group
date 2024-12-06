13cb52b26f8249b6a7d660676b61189657da510e
bek2-ful <sh.nea.yazid@gmail.com>
2024-12-06 00:44:43 +0000

    last

--- prototypes/src/main/java/com/example/prototypes/Functions.java
+++ prototypes/src/main/java/com/example/prototypes/Functions.java
@@ -1,31 +1,43 @@
 package com.example.prototypes;
 
+import javax.sql.rowset.CachedRowSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class Functions {
 
+    LocalDate localDate = LocalDate.now();
+
     public void addPoints (int userId, String dbname, String username, String password) {
-        String add = "UPDATE user_points SET current_points = current_points + 50 WHERE user_id = ?";
+        String add = "INSERT INTO transaction (user_id, transaction_date, points_earned, transaction_name) VALUES (?, ?, ?, ?)";
 
         DbFunction functions = new DbFunction();
 
-        try (Connection conn = functions.connect_to_db(dbname, username, password);
-        PreparedStatement stmt = conn.prepareStatement(add)) {
-            stmt.setInt(1,userId);
+        try (Connection conn = functions.connect_to_db(dbname, username, password)) {
 
-            int rowsUpdated = stmt.executeUpdate();
+            try (PreparedStatement addStatement = conn.prepareStatement(add)) {
+                addStatement.setInt(2, userId);
+                addStatement.setDate(3, java.sql.Date.valueOf(localDate));
+                addStatement.setInt(5, 50);
+                addStatement.setString(7, "Checked-In");
 
-            if (rowsUpdated == 1) {
-                System.out.println("Points added");
-            } else {
-                System.out.println("No points were added");
+                int rowInserted = addStatement.executeUpdate();
+
+                if (rowInserted == 1) {
+                    System.out.println("Transaction successful");
+                } else {
+                    System.out.println("Transaction failed");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
 
     }
 
